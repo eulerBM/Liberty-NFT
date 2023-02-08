be8ab3c07aaf88c170b2bd06734fde4475209d87
eulerBM <eullerborgesdamotta155@gmail.com>
2023-02-07 21:41:27 -0300

    resolvendo o bug do input

--- explorar/views.py
+++ explorar/views.py
@@ -5,23 +5,32 @@ from criar.models import *
 @login_required
 def explore(request):
     if request.method == 'GET':
+        item_all = items.objects.all()[:5]
+
+        if request.GET.get('keyword'):
+            get_search = request.GET.get('keyword')
+            item_filter = items.objects.filter(titulo=get_search)
+                      
+            context = {
+                'items_filter': item_filter,
+                'items': item_all,
+                
+            }
+            return render (request, 'explore.html', context)
+    
+        # se o imput for falso mostre isso
+
         item_all = items.objects.all()
     
-
         context = {
             'items': item_all,
 
         }
         return render (request, 'explore.html', context)
     
-    else: 
-        get_search = request.POST.get('keyword')
-        item_filter = items.objects.filter(titulo=get_search)
+    
 
-        context = {
-            'items': item_filter,
+    
+    
 
-        }
-        return render (request, 'explore.html', context)
 
-# Create your views here.
